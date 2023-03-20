d2b21030555e64a7e9b614305d71c223b857f47c
Chinmay Nagrale <30885075+MiB3Avenger@users.noreply.github.com>
2023-03-20 19:49:04 +0000

    Media handling
    Added functionality to buttons and ability to upload the post.

--- vue/wave/src/views/CreateView.vue
+++ vue/wave/src/views/CreateView.vue
@@ -1,16 +1,46 @@
+<script>
+</script>
 <script setup>
+import { ref, onMounted } from 'vue'
 import Authenticated from '../layouts/Authenticated.vue';
+
+const file = ref(null);
+const img = ref(null);
+
+const addMedia = (file) => {
+    file.click();
+}
+
+const removeMedia = () => {
+    img.value = null;
+}
+
+const changeMedia = (e) => {
+    const file = e.target.files[0];
+    img.value = URL.createObjectURL(file);
+}
+
+const uploadPost = () => {
+    console.log('uploading...');
+}
 </script>
 <template>
     <Authenticated>
         <div class="create-post">
             <div class="actions">
-                <button class="action-btn inverted"><v-icon name="md-add-round" />Add media</button>
+                <input type="file" ref="file" class="media-input" accept="image/png, image/jpeg" @change="changeMedia" />
+                <button class="action-btn inverted" @click="addMedia(file)"><v-icon name="md-add-round" />Add media</button>
+                <button class="action-btn inverted" @click="removeMedia()" v-if="img != null"><v-icon name="md-close-round" />Remove media</button>
                 <div class="bold">Create new post</div>
-                <button class="action-btn"><v-icon name="md-share-round" />Share</button>
+                <button class="action-btn" @click="uploadPost"><v-icon name="md-share-round" />Share</button>
             </div>
             <div class="create-post-container">
-                <div class="media">img</div>
+                <div class="media">
+                    <img v-if="img != null" :src="img" alt="">
+                    <div v-else>
+                        Add media to see a preview here.
+                    </div>
+                </div>
                 <div class="description">
                     <div class="caption">
                         <FormKit 
@@ -40,6 +70,9 @@ import Authenticated from '../layouts/Authenticated.vue';
         flex: 1 0 100%;
         justify-content: space-between;
         align-items: center;
+        input {
+            display: none;
+        }
         button {
             padding: 1.25rem;
             border-radius: 30px;
@@ -52,11 +85,18 @@ import Authenticated from '../layouts/Authenticated.vue';
         justify-content: space-between;
         margin-top: 2rem;
         > div {
-            flex: 1 0 auto;
+            flex: 0 0 50%;
+            width: 50%;
         }
         .media {
             margin-right: .5rem;
             border: 1px solid var(--color-border);
+            div {
+                padding: 1rem;
+            }
+            img {
+                width: 100%;
+            }
         }
         .description {
             margin-left: .5rem;
