9c1828bff817809666d29a5f6bdb17f8e17557b0
Chinmay Nagrale <30885075+MiB3Avenger@users.noreply.github.com>
2023-08-18 02:01:46 +0100

    Fixing Project 5
    Using testing environment to test with cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "./frontend/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     supportFile: "./frontend/cypress/support/e2e.js",
-    baseUrl: "http://127.0.0.1:8000",
+    baseUrl: process.env.APP_ENV == "testing" ? "http://localhost:3000" :"http://127.0.0.1:8000",
   },
 });
