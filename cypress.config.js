8ab7b92b319cf7659565ce1e7af925fc86fcc72c
Chinmay Nagrale <30885075+MiB3Avenger@users.noreply.github.com>
2023-08-18 02:17:49 +0100

    Fixing Project 5.2
    Removing environment dependency for workflow.

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "./frontend/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     supportFile: "./frontend/cypress/support/e2e.js",
-    baseUrl: process.env.APP_ENV == "local" ? "http://127.0.0.1:8000" :"http://localhost:3000",
+    baseUrl: "http://127.0.0.1:8000",
   },
 });
