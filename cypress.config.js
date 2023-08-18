9e59b4145120ba13d16b86e654ad79dba1d74da8
Chinmay Nagrale <30885075+MiB3Avenger@users.noreply.github.com>
2023-08-18 02:04:29 +0100

    Fixing Project 5.1
    Testing if we are setting app env or not.

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "./frontend/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     supportFile: "./frontend/cypress/support/e2e.js",
-    baseUrl: process.env.APP_ENV == "testing" ? "http://localhost:3000" :"http://127.0.0.1:8000",
+    baseUrl: process.env.APP_ENV == "local" ? "http://127.0.0.1:8000" :"http://localhost:3000",
   },
 });
